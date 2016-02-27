ea7afdf728791f635a06328ec87cacabbd993c15
Russell Wenban <russell@factornine.co.uk>
2016-02-27 17:58:20 +0000

    – Add `Custom Validations`

--- app.ts
+++ app.ts
@@ -6,36 +6,54 @@ import {
     FormBuilder,
     ControlGroup,
     Validators,
+    AbstractControl,
+    Control
 } from 'angular2/common';
 
 import {bootstrap} from 'angular2/platform/browser';
 
 
 /**
+ * Our custom validator
+ *
+ * A validator:
+ * - Takes a `Control` as it's input and
+ * - Returns a `StringMap<string, boolean>` where the key is "error code" and
+ *   the value is `true` if it fails
+ */
+function skuValidator(control: Control): { [s: string]: boolean } {
+    if (!control.value.match(/^\d+$/)) {
+        return {invalidSku: true};
+    }
+}
+
+
+
+/**
  * @DemoFormSkuBuilder: the top-level component for our application
  */
 @Component({
-    selector: 'demo-form-with-validations-shorthand',
+    selector: 'demo-form-with-custom-validations',
     directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
     template: `
     <div class="ui raised segment">
-        <h2 class="ui header">Form with Shorthand Validation:</h2>
+        <h2 class="ui header">Form with Custom Validations:</h2>
         <form [ngFormModel]="myForm"
             (ngSubmit)="onSubmit(myForm.value)" class="ui form">
 
 
-            <div class="field" [class.error]="!myForm.find('sku').valid && myForm.find('sku').touched">
+            <div class="field"
+                [class.error]="sku.valid && sku.touched">
                  <label for="skuInput">SKU</label>
 
                  <input type="text"
                     id="skuInput"
                     placeholder="SKU"
-                    #sku="ngForm"
-                    [ngFormControl]="myForm.controls['sku']">
+                    [ngFormControl]="sku">
 
 
-                 <div *ngIf="!sku.control.valid" class="ui error">SKU is invalid</div>
-                 <div *ngIf="sku.control.hasError('required')" class="ui error">SKU is required</div>
+                 <div *ngIf="!sku.valid" class="ui error">SKU is invalid</div>
+                 <div *ngIf="sku.hasError('invalidSku')" class="ui error">SKU must be a number</div>
 
             </div>
 
@@ -55,24 +73,33 @@ import {bootstrap} from 'angular2/platform/browser';
 
 
 
-class DemoFormWithValidationsShorthand {
+class DemoFormWithCustomValidations {
 
     myForm: ControlGroup;
+    sku: AbstractControl;
 
     constructor(fb: FormBuilder) {
 
         this.myForm = fb.group({
-            'sku': ['', Validators.required]
-        })
+            'sku': ['', Validators.compose([
+                Validators.required, skuValidator])]
+        });
+
+        this.sku = this.myForm.controls['sku'];
     }
+
+
 
     onSubmit(value:string):void {
         console.log('you submitted value: ', value);
     }
+
+
  }
 
 
-bootstrap(DemoFormWithValidationsShorthand);
+
+bootstrap(DemoFormWithCustomValidations);
 
 
 
